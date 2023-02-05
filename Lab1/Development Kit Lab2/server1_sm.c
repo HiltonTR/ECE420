41ed79f64f4886f0b4c1f87fa551f73bbc8fd1a6
stephuofa <71566670+stephuofa@users.noreply.github.com>
2023-02-04 17:35:38 -0700

    Update server1_sm.c
    functional tested version of single mutex

--- Lab1/Development Kit Lab2/server1_sm.c
+++ Lab1/Development Kit Lab2/server1_sm.c
@@ -10,28 +10,28 @@
 
 // globals for threads to share
 char** theArray; 
-// FIX ME: create single mutex for entire array
+pthread_mutex_t arrMutex;
 
 void *Worker(void *args)
 {
     int clientFileDescriptor=(int)args;
-    char msg[COM_BUFF_SIZE];
-    read(clientFileDescriptor,msg,COM_BUFF_SIZE);
-    printf("reading from client:%s\n",msg);
+    char cs_msg[COM_BUFF_SIZE];
+    read(clientFileDescriptor,cs_msg,COM_BUFF_SIZE);
 
     ClientRequest cr;
-    ParseMsg(msg, &cr);
+    ParseMsg(cs_msg, &cr);
     if (cr.is_read){
-        // FIX ME: get mutex, do reading into msg, free mutex
-        printf("read request received");
-        write(clientFileDescriptor,msg,COM_BUFF_SIZE);
+        pthread_mutex_lock(&arrMutex);
+        getContent(cs_msg,cr.pos,theArray);
+        pthread_mutex_unlock(&arrMutex);
     }
     else {
-        // FIX ME: get mutex, do writing, read string in msg, free mutex
-        printf("write request received");
-        
+        pthread_mutex_lock(&arrMutex);
+        setContent(cr.msg,cr.pos,theArray);
+        getContent(cs_msg,cr.pos,theArray);
+        pthread_mutex_unlock(&arrMutex);
     }
-    write(clientFileDescriptor,msg,COM_BUFF_SIZE);
+    write(clientFileDescriptor,cs_msg,COM_BUFF_SIZE);
     close(clientFileDescriptor);
     return NULL;
 }
@@ -47,26 +47,23 @@ int main(int argc, char* argv[])
     int n = atoi(argv[1]);
     const char* s_ip = argv[2];
     int s_port = atoi(argv[3]);
-
     printf("n is %i\n",n);
 
+
     // set up the data structure
-    
     char* initializationStr;
     initializationStr = malloc(COM_BUFF_SIZE);
     theArray = malloc(n * sizeof(char *));
     for (int i = 0; i< n; i++){
         theArray[i] = malloc(COM_BUFF_SIZE);
         char *dst = theArray[i];
-        sprintf(initializationStr, "String %i: initial value", i+1);
         int length = MIN(strlen(initializationStr)+1, COM_BUFF_SIZE);
         for (int j=0; j<length; j++){
             dst[j] = initializationStr[j];
-            printf("%c",dst[j]);
         }
-        printf("\n");
     }
     free(initializationStr);
+    pthread_mutex_init(&arrMutex, NULL);
 
 
     // set up sockets, listen and connect, spawing threads to handle new cnxs
@@ -78,6 +75,8 @@ int main(int argc, char* argv[])
     sock_var.sin_addr.s_addr=inet_addr(s_ip);
     sock_var.sin_port=s_port;
     sock_var.sin_family=AF_INET;
+    pthread_attr_t tattr;
+    pthread_attr_setdetachstate(&tattr,PTHREAD_CREATE_DETACHED); // clean up thread mem w/o explict join
     if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
     {
         printf("socket has been created\n");
@@ -87,8 +86,12 @@ int main(int argc, char* argv[])
             for(i=0;i<COM_NUM_REQUEST;i++) // allow COM_NUM_REQUEST simultaneous cnxs
             {
                 clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
-                printf("Connected to client %d\n",clientFileDescriptor);
-                pthread_create(&t[i],NULL,Worker,(void *)(long)clientFileDescriptor);
+                //printf("Connected to client %d\n",clientFileDescriptor);
+                int ptc_ret = pthread_create(&t[i],&tattr,Worker,(void *)(long)clientFileDescriptor);
+                if(ptc_ret!=0){
+                    printf("could not create thread, errno: %i\n",ptc_ret);
+                    break;
+                }
             }
         }
         close(serverFileDescriptor);
