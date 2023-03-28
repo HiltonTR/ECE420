ca48d1d903655868052eefa43a0cac9d96be56af
HiltonTR <66839605+HiltonTR@users.noreply.github.com>
2023-03-27 22:16:56 -0600

    Update main.c

--- Lab4/Development_Kit_Lab4/Development_Kit_Lab4/main.c
+++ Lab4/Development_Kit_Lab4/Development_Kit_Lab4/main.c
@@ -19,118 +19,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
+
 #include "Lab4_IO.h"
 #include "timer.h"
-#include <mpi.h>
 
 #define EPSILON 0.00001
 #define DAMPING_FACTOR 0.85
 #define THRESHOLD 0.0001
 
-int main (int argc, char* argv[]){
+
+int main(int argc, char* argv[]) {
     struct node *nodehead;
     int nodecount;
     double *r, *contribution;
     int i, j;
     double damp_const;
     int iterationcount = 0;
-    double cst_addapted_threshold;
-    double error;
 
 
     int my_rank;
-    int my_lowest_node_inc;
-    int my_highest_node_ex;
+    int my_lowest_node_inc, my_highest_node_ex;
+
     double *my_r;
     double *my_contribution;
     double *my_r_pre;
+
     int process_count;
-    int pad_add_count;
-    int padded_nodecount;
-    int nodes_per_process;
+    int nodes_per_process, padded_nodecount;
+    int continueLoop = 1;
+
     double start, end;
+
     FILE *ip;
 
     // load data
-    
     if ((ip = fopen("data_input_meta","r")) == NULL) {
         printf("Error opening the data_input_meta file.\n");
-        return 1;
+        return 254;
     }
     fscanf(ip, "%d\n", &nodecount);
     if (node_init(&nodehead, 0, nodecount)){
         printf("Error in call to node_init\n");
         return 1;
     }
-    printf("nodecount is initially: %i",nodecount);
-    
+    printf("nodecount is initially: %i\n",nodecount);
+    fclose(ip);
+
     // Start MPI and divide nodes between processes
     MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &process_count);
-    
-  
-    pad_add_count = nodecount % process_count;
-    padded_nodecount = nodecount + pad_add_count; // in case num nodes not perfectly divisible by num processes
-    nodes_per_process = padded_nodecount / process_count;
-    damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    
+    nodes_per_process = (int) (nodecount/process_count);
+    if (nodes_per_process * process_count != nodecount) {
+        nodes_per_process += 1;
+    }
+    padded_nodecount = nodes_per_process * process_count;
+    damp_const = (1.0 - DAMPING_FACTOR) / nodecount;
 
     // single process ainitializes data structures, others must wait
     r = malloc(padded_nodecount * sizeof(double));
-    contribution = malloc(padded_nodecount * sizeof(double));
-    
+
     if (my_rank == 0){
-        for ( i = 0; i < padded_nodecount; ++i)
+        for ( i = 0; i < nodecount; ++i)
             r[i] = 1.0 / nodecount;
         contribution = malloc(padded_nodecount * sizeof(double));
-        for ( i = 0; i < padded_nodecount; ++i)
+        for ( i = 0; i < nodecount; ++i)
             contribution[i] = r[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
     }
 
-    MPI_Bcast(r, padded_nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Bcast(contribution, padded_nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(r, nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(contribution, nodecount, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
 
-
     // each process computes what nodes its responsible for
-    my_lowest_node_inc = my_rank * nodes_per_process;
-    my_highest_node_ex = (my_rank+1) * nodes_per_process;
+    my_lowest_node_inc = my_rank*nodes_per_process;
+    my_highest_node_ex = (my_rank+1)*nodes_per_process - 1;
     my_r = malloc(nodes_per_process * sizeof(double));
     my_contribution = malloc(nodes_per_process * sizeof(double));
-    my_r_pre = malloc(nodes_per_process * sizeof(double));
-  
+    my_r_pre = malloc(padded_nodecount * sizeof(double));
 
     // CORE CALCULATION
     if (my_rank == 0)
         GET_TIME(start);
     do{
         ++iterationcount;
-        vec_cp(my_r, my_r_pre, nodes_per_process);
+        vec_cp(r, my_r_pre, nodecount);
+
         // update the value
-        int my_ind = 0;
-        for (i = my_lowest_node_inc; i < my_highest_node_ex; ++i){
-            my_r[my_ind] = 0;
-            for (j = 0; j < nodehead[i].num_in_links; ++j)
-                my_r[my_ind] += contribution[nodehead[i].inlinks[j]];
-            my_r[my_ind] += damp_const;
-            my_ind++;
+        for ( i = my_lowest_node_inc; i <= my_highest_node_ex && i < nodecount; ++i){
+            my_r[i-my_lowest_node_inc] = 0;
+            for ( j = 0; j < nodehead[i].num_in_links; ++j)
+                my_r[i-my_lowest_node_inc] += contribution[nodehead[i].inlinks[j]];
+            my_r[i-my_lowest_node_inc] += damp_const;
         }
+
         // update and broadcast the contribution
-        my_ind = 0;
-        for (i = my_lowest_node_inc; i < my_highest_node_ex; ++i){
-            my_contribution[my_ind] = r[i] / nodehead[i].num_out_links * DAMPING_FACTOR;
-            my_ind++;
+        for ( i=my_lowest_node_inc; i <= my_highest_node_ex && i<nodecount; ++i){
+            my_contribution[i-my_lowest_node_inc] = my_r[i-my_lowest_node_inc] / nodehead[i].num_out_links * DAMPING_FACTOR;
         }
-        // update global r, contribution
-        MPI_Allgather(my_r, nodes_per_process, MPI_DOUBLE, r, nodes_per_process, MPI_DOUBLE, MPI_COMM_WORLD);
+
+        MPI_Gather(my_r, nodes_per_process, MPI_DOUBLE, r, nodes_per_process, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         MPI_Allgather(my_contribution, nodes_per_process, MPI_DOUBLE, contribution, nodes_per_process, MPI_DOUBLE, MPI_COMM_WORLD);
         // wait for all processes to update globals before continuing
         MPI_Barrier(MPI_COMM_WORLD);
-    
-    }while(rel_error(my_r, my_r_pre, nodes_per_process) >= EPSILON);
-    
+
+
+    }while(rel_error(r, my_r_pre, nodes_per_process) >= EPSILON);
+
     // wait for all processes to finish
     MPI_Barrier(MPI_COMM_WORLD);
     
@@ -147,6 +144,6 @@ int main (int argc, char* argv[]){
     MPI_Finalize();
     node_destroy(nodehead, nodecount);
     free(contribution);
-
     return 0;
 }
+
